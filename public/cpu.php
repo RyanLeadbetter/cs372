c89e991580507be3ef166a00e575ea5abd2bd0a7
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-03-24 20:24:34 -0600

    Update cpu.php
    update links

--- public/cpu.php
+++ public/cpu.php
@@ -1,7 +1,7 @@
 <?php 
 session_start();     
 if (!isset($_SESSION['email']) || $_SESSION['email'] == '' ) {
-    header("Location: http://localhost/Chess_LoginPage.php");
+    header("Location: Chess_LoginPage.php");
 }
 ?>
 <!DOCTYPE html>
@@ -38,7 +38,7 @@ if (!isset($_SESSION['email']) || $_SESSION['email'] == '' ) {
 <div id="lightbox", class="modal" style='display:none; position:fixed; padding-top: 100px; left: 25%; top: 25%; width: 50%; height: 25%; background-color: white; box-shadow:0 0 0 1600px rgba(0,0,0,0.55);'>
     <div class="modal-content" style='width: 50%; margin: 0 auto;'>
         <h1 style="font-size: 12pt; display:none;" id="message"></h1>
-        <a class="btn" type="button" href="http://localhost/Welcome.php">Ok</a>
+        <a class="btn" type="button" href="Welcome.php">Ok</a>
     </div>
 </div>
 <script src="lib/jquery/jquery-3.2.1.min.js"></script>

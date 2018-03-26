78a1c1f02b8f2e2e5bdad07b3f54751730c178a7
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-03-26 16:13:49 -0600

    Update pvp.php

--- public/pvp.php
+++ public/pvp.php
@@ -48,7 +48,7 @@ if (isset($_SESSION['email'])) {
 ?>
     <div class="page login" id='page-login'>
       <!--<div class='logo'></div>-->
-      <input id='username' value="<?php echo $name; ?>" ></input>
+      <input id='username' value="" ></input>
       <button id='login'>Find opponent</button>
     </div>
     <div class="page lobby" id='page-lobby'>

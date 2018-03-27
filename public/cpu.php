8f9756469a9e11cdc89c5be63f18e0c88785111f
UlisesZes <37345455+UlisesZes@users.noreply.github.com>
2018-03-27 17:25:55 -0600

    Update cpu.php

--- public/cpu.php
+++ public/cpu.php
@@ -17,7 +17,7 @@ if (!isset($_SESSION['email']) || $_SESSION['email'] == '' ) {
         <div class="container text-center" style="background:none;">
             <br>
             <h1 class="tittle">The <br>Chess Game</h1>
-            <!--<button class="btn btn-default">About</button>-->
+            <button class="btn btn-default" onclick="window.open('https://en.wikipedia.org/wiki/Rules_of_chess'); window.location.href = 'https://en.wikipedia.org/wiki/Rules_of_chess'" target="_blank">Don't know what to do? See the rules</button>
         </div>
 </div>
 
